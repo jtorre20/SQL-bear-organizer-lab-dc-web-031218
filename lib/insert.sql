e658d8bcdf61cd169535d9b7230971de8fc10c5b
jtorre20 <jtorres1989@gmail.com>
2018-03-20 13:50:20 -0400

    selected all females

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3 , "M", "Brown", "Choclatey", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10 , "M", "Brown", "Rowdy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7 , "F", "Brown", "Relaxed", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1 , "M", "White", "Commanding", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4 , "F", "Brown", "Lazy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3 , "M", "Black", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6 , "F", "Brown", "Choclatey", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3 , "F", "Black", NULL, 1);
